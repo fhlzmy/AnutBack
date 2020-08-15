ef375d10ba712ae98036f9752a012f2fbb0a9c32
yueyuedajiejie <504349089@qq.com>
2020-08-15 21:14:10 +0800

    加了一丢丢的料.ps：主要是想同步一下家里台式机上代码和Mac之间的代码差别

--- src/main/java/com/fhlzmy/web/util/FToken.java
+++ src/main/java/com/fhlzmy/web/util/FToken.java
@@ -1,5 +1,14 @@
 package com.fhlzmy.web.util;
 
 
+import java.util.UUID;
+
 public class FToken {
+
+
+    public static String getToken(){
+        return UUID.randomUUID().toString();
+    }
+
+
 }
